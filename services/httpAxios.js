acd9a316c33539eab9cb996d00e315c5f8d75746
ngocduy2004 <ngocduy6379@gmail.com>
2026-03-22 23:41:55 +0700

    update

--- services/httpAxios.js
+++ services/httpAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost/CDTT_Backend/public/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost/CDTT_Backend/public/api/",
 });
 
 api.interceptors.response.use(
